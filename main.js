b7a69e25f8ec080d9c42185b914f154a61c91eef
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-05-16 20:04:36 +0200

    exercise 1: palindrome - fix

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-// Chiedo all'utente = prompt
+// Chiedo all'utente = uso un prompt
 
-isPalindrome(prompt("Inserisci una parola")); // Note: function is declared below (hoisting)
+console.log(isPalindrome(prompt("Inserisci una parola"))); // Note: function is declared below (hoisting)
 
-// Parola palindroma = uguale se letta al contrario
+// Parola palindroma = uguale se letta al contrario // function will have to return a boolean value
 
 function isPalindrome (string) {
     let reverted = string.split("").toReversed().join("");
-    let isPalindrome = (string === reverted);
-    return console.log (isPalindrome);
+    let check = (string === reverted);
+    return check;
 };
